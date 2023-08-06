4c5aae45aa96a493bdf8ece7798a92d9ae743849
laof <345603828@qq.com>
2023-08-06 11:25:54 +0800

    ae

--- test/main.go
+++ test/main.go
@@ -1,10 +1,7 @@
 package main
 
-var sss = `
-`
-
 func main() {
-	// dd, _ := data.Get("https://laof.github.io/get-nodes-test-app/json/data.json", true, 0)
+	// dd, _ := data.Get("n", true, 0)
 	// fmt.Println(dd.SuccessNodes)
 	// fmt.Println(dd.ErrorServers)
 }
